550b9b9d5b22bb3bc8d1248190651acd73927914
Catrin <catrinhughes92@gmail.com>
2020-01-13 13:44:04 +0000

    created manager class

--- src/main/java/staff/management/Manager.java
+++ src/main/java/staff/management/Manager.java
@@ -1,4 +1,17 @@
 package staff.management;
 
-public class Manager {
+import staff.Employee;
+
+public class Manager extends Employee {
+
+    private String depName;
+
+    public Manager(String name, String NINumber, double salary, String depName) {
+        super(name, NINumber, salary);
+        this.depName = depName;
+    }
+
+    public String getDepName() {
+        return this.depName;
+    }
 }
