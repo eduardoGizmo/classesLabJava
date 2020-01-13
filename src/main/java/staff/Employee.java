057db93448322d550e1c655493e557e29b4746da
Catrin <catrinhughes92@gmail.com>
2020-01-13 13:40:03 +0000

    created employee abstract class

--- src/main/java/staff/Employee.java
+++ src/main/java/staff/Employee.java
@@ -1,4 +1,35 @@
 package staff;
 
-public class Employee {
+public abstract class Employee {
+
+    private String name;
+    private String NINumber;
+    private double salary;
+
+    public Employee(String name, String NINumber, double salary) {
+        this.name = name;
+        this.NINumber = NINumber;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String NINumber() {
+        return this.NINumber;
+    }
+
+    public double salary() {
+        return this.salary;
+    }
+
+    public void raiseSalary(double salary) {
+        this.salary += salary;
+    }
+
+    public double payBonus() {
+        double percentage = this.salary / 100;
+        return percentage;
+    }
 }
