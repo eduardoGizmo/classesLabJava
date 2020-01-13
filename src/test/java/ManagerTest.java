4434ab3ddb372b020beb3e5d5c431bc8f60e2bfc
Catrin <catrinhughes92@gmail.com>
2020-01-13 13:50:43 +0000

    get name test passed

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -1,4 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.management.Manager;
+
+import static org.junit.Assert.assertEquals;
+
 public class ManagerTest {
 
+    private Manager manager;
 
+    @Before
+    public void before() {
+        manager = new Manager("Catrin", "J63874V", 60000.00, "General");
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Catrin", manager.getName());
+    }
 }
