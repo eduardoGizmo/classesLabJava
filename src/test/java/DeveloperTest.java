29e74ee72f0c4ad0cda8af9393a246a9c1bf5a5e
Catrin <catrinhughes92@gmail.com>
2020-01-13 14:15:53 +0000

    get name passed for developer

--- src/test/java/DeveloperTest.java
+++ src/test/java/DeveloperTest.java
@@ -1,6 +1,22 @@
-import
+import org.junit.Before;
+import org.junit.Test;
+import staff.techStaff.Developer;
+
+import static org.junit.Assert.assertEquals;
 
 public class DeveloperTest {
 
     private Developer developer;
+
+    @Before
+    public void before() {
+        developer = new Developer("Eduardo", "E23455C", 50000.00);
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Eduardo", developer.getName());
+    }
+
+
 }
